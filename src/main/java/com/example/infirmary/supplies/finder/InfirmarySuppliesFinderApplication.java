96aad0f998eefa012be75a264d2103fa53cf215d
saway <sawa.y918@gmail.com>
2025-05-19 17:56:47 +0900

    initial commit: spring initializrから構成ファイルをダウンロードしてビルド

---
+++ src/main/java/com/example/infirmary/supplies/finder/InfirmarySuppliesFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.infirmary.supplies.finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfirmarySuppliesFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfirmarySuppliesFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/infirmary/supplies/finder/InfirmarySuppliesFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infirmary.supplies.finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfirmarySuppliesFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
